f7c8b19b3861671bd25a660a8afec9bf79ca9f51
taoyuan <torworx@gmail.com>
2016-01-02 23:26:17 +0800

    Added MQTT client and server

---
+++ test/mqtt.client-server.test.js
@@ -0,0 +1,71 @@
+"use strict";
+
+var should = require('should');
+var _ = require('lodash');
+var jayson = require(__dirname + '/../');
+var support = require(__dirname + '/support');
+var common = support.common;
+var net = require('net');
+var url = require('url');
+
+var Counter = require('./support/counter');
+var coder = {
+  code: 0x42,
+  type: Counter,
+  encode: function (obj) {
+    var buf = new Buffer(4);
+    buf.writeInt32BE(obj.count, 0);
+    return buf;
+  },
+  decode: function (data) {
+    return new Counter(data.readInt32BE(0));
+  }
+};
+
+describe('Jayson.Mqtt', function () {
+
+  describe('integration', function () {
+
+    var options = {
+      topic: '$foo',
+      coder: coder
+    };
+
+    var mqttserver = buildMqttServer();
+    var server = jayson.server(support.server.methods, support.server.options).mqtt(mqttserver.url, options);
+    var client = jayson.client.mqtt(mqttserver.url, options);
+
+    before(function (done) {
+      server.ready(function () {
+        client.ready(done);
+      });
+    });
+
+    after(function (done) {
+      client.close(function () {
+        server.close(function () {
+          mqttserver.close(done);
+        });
+      });
+    });
+
+    describe('common tests', common(client));
+  });
+
+});
+
+function buildMqttServer(port, cb) {
+  if (typeof port === 'function') {
+    cb = port;
+    port = undefined;
+  }
+
+  port = port || 43210;
+
+  var Server = require('mosca').Server;
+  var server = new Server({port: port}, cb);
+  server.url = 'mqtt://localhost:' + port;
+  server.port = port;
+  return server;
+}
+
